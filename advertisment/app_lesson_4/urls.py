b8d4d8db2aea289fa24f85fe05ad5edfa610fd98
Ирия <mrdidnew@gmail.com>
2023-08-12 18:12:13 +0800

    Обновление template

---
+++ advertisment/app_lesson_4/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import index,top_sellers,post_ads,registr,login,profile
+
+urlpatterns = [path("",index,name="main"),path("top-sellers",top_sellers,name="tp"),
+               path("advertisement-post",post_ads,name="adv"),path("register",registr,name="rg"),
+               path("login",login,name="log"),path("profile",profile,name="pf")]
